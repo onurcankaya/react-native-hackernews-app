434895d3e6d42cada928bac9e2f9db6995502fd5
onurcankaya <onur.eren.cankaya@gmail.com>
2019-02-10 13:52:56 +0100

    react-native-navigation initial ios setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-/**
- * @format
- * @lint-ignore-every XPLATJSCOPYRIGHT1
- */
+import { Navigation } from 'react-native-navigation'
 
-import { AppRegistry } from 'react-native'
 import App from './App'
-import { name as appName } from './app.json'
 
-AppRegistry.registerComponent(appName, () => App)
+Navigation.registerComponent(`navigation.playground.WelcomeScreen`, () => App)
+
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      component: {
+        name: 'navigation.playground.WelcomeScreen',
+      },
+    },
+  })
+})
--- ios/build/Build/Intermediates.noindex/hackernews.build/Debug-iphonesimulator/hackernews.build/Objects-normal/x86_64/AppDelegate.d
+++ ios/build/Build/Intermediates.noindex/hackernews.build/Debug-iphonesimulator/hackernews.build/Objects-normal/x86_64/AppDelegate.d
@@ -9,4 +9,22 @@ dependencies: \
   /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTDefines.h \
   /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBridgeModule.h \
   /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTFrameUpdate.h \
-  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTInvalidating.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/ReactNativeNavigation/ReactNativeNavigation.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUIManager.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTViewManager.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTConvert.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAnimationType.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTBorderStyle.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTLog.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTAssert.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTUtils.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTPointerEvents.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTTextDecorationLineType.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/Yoga.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGEnums.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/yoga/YGMacros.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTEventDispatcher.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/UIView+React.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/React/RCTComponent.h \
+  /Users/onur/Code/onur/react-native-hackernews-app/ios/build/Build/Products/Debug-iphonesimulator/include/ReactNativeNavigation/RNNBridgeManagerDelegate.h
